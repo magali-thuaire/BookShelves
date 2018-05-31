516194dd5fdd78270e186d4a13efb6a768e8f95f
Magali <magali_thuaire@yahoo.fr>
2018-05-31 16:37:37 +0200

    ajout des modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,44 @@ import { BookListComponent } from './book-list/book-list.component';
 import { SingleBookComponent } from './book-list/single-book/single-book.component';
 import { BookFormComponent } from './book-list/book-form/book-form.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthService } from './services/auth.service';
+import { BooksService } from './services/books.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+	{ path: 'auth/signup', component: SignupComponent },
+	{ path: 'auth/signin', component: SigninComponent },
+	{ path: 'books', component: BookListComponent },
+	{ path: 'books/new', component: BookFormComponent },
+	{ path: 'books/view/:id', component: SingleBookComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    SigninComponent,
-    BookListComponent,
-    SingleBookComponent,
-    BookFormComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		SignupComponent,
+		SigninComponent,
+		BookListComponent,
+		SingleBookComponent,
+		BookFormComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+		RouterModule,
+		RouterModule.forRoot(appRoutes)
+	],
+	providers: [
+		AuthService,
+		BooksService,
+		AuthGuardService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
