44c54d8d43d4e1dea0d65ec3828483a9993ee1ad
Magali <magali_thuaire@yahoo.fr>
2018-05-31 16:56:52 +0200

    ajout firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	constructor() {
+		var config = {
+			apiKey: "AIzaSyAxBwXB0SLOQcCfiRvo_pEw6e5X6HOjSI8",
+			authDomain: "bookshelves-dab6b.firebaseapp.com",
+			databaseURL: "https://bookshelves-dab6b.firebaseio.com",
+			projectId: "bookshelves-dab6b",
+			storageBucket: "bookshelves-dab6b.appspot.com",
+			messagingSenderId: "117552658411"
+		};
+		firebase.initializeApp(config);
+	}
 }
