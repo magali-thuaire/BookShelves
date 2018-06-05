f1393ad1f9e9895a71e238b9d63a3da3b9fd501a
Magali <magali_thuaire@yahoo.fr>
2018-06-05 16:24:13 +0200

    ajout fonctions authentification
    Fonctions :
    - createNewUser
    - signIn
    - signOut

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { reject, resolve } from 'q';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+	constructor() { }
+
+	// Création d'un nouvel utilisateur
+	createNewUser(email: string, password: string) {
+		return new Promise(
+			(resolve, reject) => {
+				firebase.auth().createUserWithEmailAndPassword(email, password).then(
+					() => {
+						resolve();
+					},
+					(error) => {
+						reject(error);
+					}
+				)
+			}
+    	)
+	}
+
+	// Connexion d'un utilisateur
+	signInUser(email:string, password: string) {
+		return new Promise(
+			(resolve, reject) => {
+				firebase.auth().signInWithEmailAndPassword(email, password).then(
+					()=> {
+						resolve();
+					},
+					(error)=> {
+						reject(error);
+					}
+				)
+			}
+		)
+	}
+
+	// Déconnexion d'un utilisateur
+	signOut() {
+		firebase.auth().signOut();
+	}
 }
