e2fa4e22b2b536e9f34970c99efb0f13d6738788
Magali <magali_thuaire@yahoo.fr>
2018-06-09 18:06:35 +0200

    ajout des vues du CRUD des livres

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BooksService } from '../../services/books.service';
+import { Router } from '@angular/router';
+import { Book } from '../../models/Book.model';
 
 @Component({
-  selector: 'app-book-form',
-  templateUrl: './book-form.component.html',
-  styleUrls: ['./book-form.component.scss']
+    selector: 'app-book-form',
+    templateUrl: './book-form.component.html',
+    styleUrls: ['./book-form.component.scss']
 })
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+    bookForm: FormGroup;
 
-  ngOnInit() {
-  }
 
+    constructor(private formBuilder: FormBuilder,
+                private booksService: BooksService,
+                private router: Router) { }
+
+    ngOnInit() {
+        this.initForm();
+    }
+
+    initForm() {
+        this.bookForm = this.formBuilder.group({
+            title: ['', Validators.required],
+            author: ['', Validators.required]
+        });
+    }
+
+    onSaveBook() {
+        const title = this.bookForm.get('title').value;
+        const author = this.bookForm.get('author').value;
+        const newBook = new Book(title, author);
+        this.booksService.createNewBook(newBook);
+        this.router.navigate(['/books']);
+    }
 }
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
+import { Book } from '../models/Book.model';
 
 @Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+    selector: 'app-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    books: Book[];
+    booksSubcription: Subscription;
 
-  ngOnInit() {
-  }
+    constructor(private booksService: BooksService, private router: Router) { }
+
+    ngOnInit() {
+        this.booksSubcription = this.booksService.booksSubject.subscribe(
+            (books: Book[]) => {
+                this.books = books;
+            }
+        )
+        this.booksService.emitBooks();
+    }
+
+    onNewBook() {
+        this.router.navigate(['/books', 'new']);
+    }
+
+    onDeleteBook(book: Book) {
+        this.booksService.removeBook(book);
+    }
+
+    onViewBook(id: number) {
+        this.router.navigate(['/books', 'view', id]);
+    }
+
+    ngOnDestroy() {
+        this.booksSubcription.unsubscribe();
+    }
 
 }
