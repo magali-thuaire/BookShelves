16fd1d46d4d4efc89e62d359fb4f0381a1594bc3
Unknown <magali_thuaire@yahoo.fr>
2018-09-18 13:04:29 +0200

    ajout du modèle Book

---
+++ src/app/models/Book.model.ts
@@ -0,0 +1,14 @@
+export class Book {
+
+  constructor(
+    // Titre du livre
+    public title: string,
+    // Auteur du livre
+    public author: string,
+    // URL de la photo : facultatif
+    public photo?: string
+
+  ) {
+  }
+
+}
