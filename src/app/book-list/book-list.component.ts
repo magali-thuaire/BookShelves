2e96013b06ddcdba68f4b9bbfd3e26aa476e6a1a
Unknown <magali_thuaire@yahoo.fr>
2018-09-18 16:17:28 +0200

    affichage de la liste des livres

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from "../services/book.service";
+import { Book } from "../models/Book.model";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
 
-  constructor() { }
+  // Tableau des livres
+  books: Book[];
+  // Souscription au tableau privé des livres
+  booksSubscription: Subscription;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    // Initialisation de la souscription au tableau privé des livres
+    this.booksSubscription = this.bookService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    // Emission du tableau des livres
+    this.bookService.emitBooks();
+  }
 
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,4 +1,24 @@
+import { Book } from "../models/Book.model";
+import { Subject } from "rxjs";
+
+
 export class BookService {
 
+  // Tableau privé des livres
+  private books: Book[] = [
+    {
+      title: 'Titre',
+      author: 'Auteur'
+    }
+  ];
+
+  // Subject pour le tableau privé des livres
+  booksSubject = new Subject<Book[]>();
+
   constructor() { }
+
+  // Emission du tableau des livres
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
 }
